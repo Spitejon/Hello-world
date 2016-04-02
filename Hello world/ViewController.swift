4cf7bfbef835fb7242251ee454388935a14fb127
Evgeny <spitejon@gmail.com>
2016-04-02 14:44:24 +0300

    Первый проект :) Первый commit

--- Hello world/ViewController.swift
+++ Hello world/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    @IBOutlet weak var LabelField: UILabel!
+
+    @IBOutlet weak var Resault: UILabel!
+    @IBOutlet weak var TextInput: UITextField!
+    @IBAction func ActButton(sender: AnyObject) {
+      
+      Resault.text = TextInput.text
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
